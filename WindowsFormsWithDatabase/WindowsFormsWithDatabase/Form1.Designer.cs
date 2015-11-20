c40d96ff30ef44a6a6af749a7f736e48310eaa81
DominikStyp <dominik.stypula1@gmail.com>
2015-11-20 05:30:24 +0100

    WORKING (with only Constraints problem while adding/removing list)

--- WindowsFormsWithDatabase/WindowsFormsWithDatabase/Form1.Designer.cs
+++ WindowsFormsWithDatabase/WindowsFormsWithDatabase/Form1.Designer.cs
@@ -28,13 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.itemsList = new System.Windows.Forms.ListBox();
+            this.listsList = new System.Windows.Forms.ListBox();
+            this.AddNewItem = new System.Windows.Forms.Button();
+            this.AddNewList = new System.Windows.Forms.Button();
+            this.removeSelectedItem = new System.Windows.Forms.Button();
+            this.removeSelectedList = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.itemsTableAdapter = new WindowsFormsWithDatabase.DataSet1TableAdapters.ItemsTableAdapter();
+            this.listsTableAdapter = new WindowsFormsWithDatabase.DataSet1TableAdapters.ListsTableAdapter();
+            this.tableAdapterManager = new WindowsFormsWithDatabase.DataSet1TableAdapters.TableAdapterManager();
+            this.itemsToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.itemsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.listsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new WindowsFormsWithDatabase.DataSet1();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // itemsList
+            // 
+            this.itemsList.DataSource = this.itemsBindingSource;
+            this.itemsList.DisplayMember = "Name";
+            this.itemsList.FormattingEnabled = true;
+            this.itemsList.Location = new System.Drawing.Point(12, 62);
+            this.itemsList.Name = "itemsList";
+            this.itemsList.Size = new System.Drawing.Size(142, 316);
+            this.itemsList.TabIndex = 0;
+            // 
+            // listsList
+            // 
+            this.listsList.DataSource = this.listsBindingSource;
+            this.listsList.DisplayMember = "Name";
+            this.listsList.FormattingEnabled = true;
+            this.listsList.Location = new System.Drawing.Point(334, 62);
+            this.listsList.Name = "listsList";
+            this.listsList.Size = new System.Drawing.Size(138, 316);
+            this.listsList.TabIndex = 1;
+            this.listsList.SelectedIndexChanged += new System.EventHandler(this.listsList_SelectedIndexChanged);
+            this.listsList.MouseHover += new System.EventHandler(this.listsList_MouseHover);
+            // 
+            // AddNewItem
+            // 
+            this.AddNewItem.Location = new System.Drawing.Point(13, 385);
+            this.AddNewItem.Name = "AddNewItem";
+            this.AddNewItem.Size = new System.Drawing.Size(97, 23);
+            this.AddNewItem.TabIndex = 2;
+            this.AddNewItem.Text = "Add new item";
+            this.AddNewItem.UseVisualStyleBackColor = true;
+            this.AddNewItem.Click += new System.EventHandler(this.AddNewItem_Click);
+            // 
+            // AddNewList
+            // 
+            this.AddNewList.Location = new System.Drawing.Point(397, 385);
+            this.AddNewList.Name = "AddNewList";
+            this.AddNewList.Size = new System.Drawing.Size(75, 23);
+            this.AddNewList.TabIndex = 3;
+            this.AddNewList.Text = "Add new list";
+            this.AddNewList.UseVisualStyleBackColor = true;
+            this.AddNewList.Click += new System.EventHandler(this.AddNewList_Click);
+            // 
+            // removeSelectedItem
+            // 
+            this.removeSelectedItem.Location = new System.Drawing.Point(13, 414);
+            this.removeSelectedItem.Name = "removeSelectedItem";
+            this.removeSelectedItem.Size = new System.Drawing.Size(125, 23);
+            this.removeSelectedItem.TabIndex = 4;
+            this.removeSelectedItem.Text = "Remove selected item";
+            this.removeSelectedItem.UseVisualStyleBackColor = true;
+            this.removeSelectedItem.Click += new System.EventHandler(this.removeSelectedItem_Click);
+            // 
+            // removeSelectedList
+            // 
+            this.removeSelectedList.Location = new System.Drawing.Point(363, 414);
+            this.removeSelectedList.Name = "removeSelectedList";
+            this.removeSelectedList.Size = new System.Drawing.Size(109, 23);
+            this.removeSelectedList.TabIndex = 5;
+            this.removeSelectedList.Text = "Remove selected list";
+            this.removeSelectedList.UseVisualStyleBackColor = true;
+            this.removeSelectedList.Click += new System.EventHandler(this.removeSelectedList_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Add new item:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(331, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Add new list:";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // itemsTableAdapter
+            // 
+            this.itemsTableAdapter.ClearBeforeFill = true;
+            // 
+            // listsTableAdapter
+            // 
+            this.listsTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.ItemsTableAdapter = this.itemsTableAdapter;
+            this.tableAdapterManager.ListsTableAdapter = this.listsTableAdapter;
+            this.tableAdapterManager.UpdateOrder = WindowsFormsWithDatabase.DataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // itemsBindingSource
+            // 
+            this.itemsBindingSource.DataMember = "Items";
+            this.itemsBindingSource.DataSource = this.dataSet1;
+            // 
+            // listsBindingSource
+            // 
+            this.listsBindingSource.DataMember = "Lists";
+            this.listsBindingSource.DataSource = this.dataSet1;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(484, 482);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.removeSelectedList);
+            this.Controls.Add(this.removeSelectedItem);
+            this.Controls.Add(this.AddNewList);
+            this.Controls.Add(this.AddNewItem);
+            this.Controls.Add(this.listsList);
+            this.Controls.Add(this.itemsList);
             this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.MyForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.itemsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -42,20 +194,25 @@
 
         private System.Windows.Forms.ListBox itemsList;
         private System.Windows.Forms.ListBox listsList;
-        private System.Windows.Forms.BindingSource itemsBindingSource;
-        public DataSet1TableAdapters.ItemsTableAdapter itemsTableAdapter;
-        public DataSet1TableAdapters.TableAdapterManager tableAdapterManager;
-        private System.Windows.Forms.Button AddNewItem;
-        private System.Windows.Forms.Button AddNewList;
-        public DataSet1 dataSet1;
-        private DataSet1TableAdapters.ListsTableAdapter listsTableAdapter;
-        private System.Windows.Forms.BindingSource listsBindingSource;
+        //labels 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ToolTip itemsToolTip;
+        // buttons
+        private System.Windows.Forms.Button AddNewItem;
+        private System.Windows.Forms.Button AddNewList;
         private System.Windows.Forms.Button removeSelectedItem;
         private System.Windows.Forms.Button removeSelectedList;
+        // tray icon & tooltips
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolTip itemsToolTip;
+        // table adapters
+        private DataSet1TableAdapters.ItemsTableAdapter itemsTableAdapter;
+        private DataSet1TableAdapters.ListsTableAdapter listsTableAdapter;
+        private DataSet1TableAdapters.TableAdapterManager tableAdapterManager;
+        // database binding sources
+        private System.Windows.Forms.BindingSource itemsBindingSource;
+        private System.Windows.Forms.BindingSource listsBindingSource;
+        private DataSet1 dataSet1;
     }
 }
 
--- WindowsFormsWithDatabase/WindowsFormsWithDatabase/Form1.cs
+++ WindowsFormsWithDatabase/WindowsFormsWithDatabase/Form1.cs
@@ -63,14 +63,10 @@ namespace WindowsFormsWithDatabase
             listsList.DataSource = listsBindingSource;
             // Due to DataTable throws InvalidConstraintException on .Fill (Foreign key problem)
             this.dataSet1.Lists.ChildRelations.Clear();
+            this.dataSet1.Lists.Constraints.Clear();
             this.listsTableAdapter.Fill(this.dataSet1.Lists);
         }
-
-        private void listsList_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            refreshItemsList();
-        }
-        
+ 
         public byte getCheckedListId()
         {
             String valueOfItem = "";
@@ -99,106 +95,17 @@ namespace WindowsFormsWithDatabase
             return Int32.Parse(valueOfItem);
         }
 
-        private void AddNewList_Click(object sender, EventArgs e)
-        {
-            new AddNewList(this).Show();
-        }
-
-        private void AddNewItem_Click(object sender, EventArgs e)
-        {
-            if(currentCheckedListId < 0)
-            {
-                MessageBox.Show("First you have to choose list to add an item.\nIf there is none, you must create it.");
-                return;
-            }
-            new AddNewItem(this).Show();
-        }
-
         private void d(object d)
         {
             MessageBox.Show(d.ToString());
         }
-
-        private void itemsList_MouseHover(object sender, EventArgs e)
-        {
-            itemsToolTip.Show("List of Items.\nEvery item must be assigned to one list.\nFirst you have to create and choose list to add new item.", itemsList);
-        }
-
-        private void listsList_MouseHover(object sender, EventArgs e)
-        {
-            itemsToolTip.Show("List of items lists.\nThere must be at least one list if you wan't to add an item.", listsList);
-        }
-
 
         private bool confirmBox(string title, string text)
         {
             DialogResult dr = MessageBox.Show(text, title, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
             return dr == DialogResult.Yes;
         }
-
-        private void removeSelectedItem_Click(object sender, EventArgs e)
-        {
-            int id = getCheckedItemId();
-            if(id < 1)
-            {
-                MessageBox.Show("Please choose an item");
-                return;
-            }
-            if(confirmBox("Delete an item", "Do you want to remove this item from database?"))
-            {
-                manipulatingDatabase.removeItemsRow(id);
-                refreshItemsList();
-            }
-        }
-
-        private void removeSelectedList_Click(object sender, EventArgs e)
-        {
-            byte id = getCheckedListId();
-            if (id < 1)
-            {
-                MessageBox.Show("Please choose a list");
-                return;
-            }
-            if (confirmBox("Delete whole list", "Do you want to remove this list (and items binded to it) from database?"))
-            {
-                manipulatingDatabase.removeListsRow(id);
-                refreshListsList();
-                refreshItemsList();
-            }
-        }
-
-
-        /******************************** TRAY ICON **********************/
-        /******************************** TRAY ICON **********************/
-        /******************************** TRAY ICON **********************/
-        /******************************** TRAY ICON **********************/
-
-        //  The NotifyIcon object
-        // private System.Windows.Forms.NotifyIcon notifyIcon1;
-
-
-
-        // REMEMBER: First you have to define Resize event in Designer
-        private void MyForm_Resize(object sender, EventArgs e)
-        {
-            notifyIcon1.BalloonTipTitle = "Minimize to Tray App";
-            notifyIcon1.BalloonTipText = "You have successfully minimized your form.";
-            if (FormWindowState.Minimized == this.WindowState)
-            {
-                notifyIcon1.Visible = true;
-                notifyIcon1.ShowBalloonTip(500);
-                this.Hide();
-            }
-            else if (FormWindowState.Normal == this.WindowState)
-            {
-                notifyIcon1.Visible = false;
-            }
-        }
-
-        private void notifyIcon1_MouseDoubleClick_1(object sender, MouseEventArgs e)
-        {
-            this.Show();
-            this.WindowState = FormWindowState.Normal;
-        }
     }
+
+
 }
